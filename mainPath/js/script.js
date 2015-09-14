6332880f291ee7f4b066ef2d1467b95842d055a1
Ly0w <kondratovolexiy@gmail.com>
2015-09-14 17:31:51 +0300

    Update script.js
    Realize logic validation: Validate required.

--- mainPath/js/script.js
+++ mainPath/js/script.js
@@ -12,10 +12,9 @@ $(document).ready(function () {
         return false;
     });
 });
-$(document).ready(function (){
+$(document).ready(function () {
     $(document).on("click", ".tabs .tab-nav a", function () {
         var contentId = this.hash;
-
         $('.tab-nav a').removeClass("active");
         $(this).addClass("active");
 
@@ -26,19 +25,7 @@ $(document).ready(function (){
     });
 });
 
-$(document).ready(function () {
-    $(document).on("click", ".loginbutton", function () {
-        $('   <tr>                    <td>' + $('#textname').val() + '</td>                    <td>' + $('#textlogin').val() + '</td>                    <td>' + $('#textemail').val() + '</td>                </tr>').insertAfter(".insert");
-        var contentId = '#section1';
-        $('.tab-nav a').removeClass("active");
-        $('.tabs .tab-nav a[href=#section1]').addClass("active");
 
-        $('.tab-content .section').hide();
-
-        $(contentId).fadeIn();
-        $(".menu").fadeIn();
-    });
-});
 
 $(document).ready(function () {
     $(document).on("click", "#loginright", function () {
@@ -47,3 +34,45 @@ $(document).ready(function () {
         return false;
     });
 });
+
+$(function () {
+    $('.loginbutton').click(function (event) {
+        event.preventDefault();
+        var inputs = $('input');
+        var isError = false;
+
+        $('.inputframe').removeClass('error');
+        $('.error-data').remove();
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            if ($(input).hasClass('logininput') && !validateRequired($(input).val())) {
+                addErrorData($(input), "Заполните поле");
+                isError = true;
+            }
+        }
+        if (isError === false) {
+
+            $('   <tr>                    <td>' + $('#textname').val() + '</td>                    <td>' + $('#textlogin').val() + '</td>                    <td>' + $('#textemail').val() + '</td>                </tr>').insertAfter(".insert");
+            var contentId = '#section1';
+            $('.tab-nav a').removeClass("active");
+            $('.tabs .tab-nav a[href=#section1]').addClass("active");
+
+            $('.tab-content .section').hide();
+
+            $(contentId).fadeIn();
+            $(".menu").fadeIn();
+        }
+    });
+});
+
+function validateRequired(value) {
+    var bool = true;
+    if (value === "") bool = false;
+
+    return bool;
+}
+
+function addErrorData(element, error) {
+    element.parent().addClass("error");
+    element.after("<div class='error-data'>" + error + "</div>");
+}
